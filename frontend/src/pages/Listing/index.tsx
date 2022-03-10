1348e7e3376c4a666411de266c757fd13c3ce6e5
Juliano Carlos de Oliveira <jcmsx@yahoo.com.br>
2022-03-09 21:51:35 -0300

    Movie Score

--- frontend/src/pages/Listing/index.tsx
+++ frontend/src/pages/Listing/index.tsx
@@ -1,12 +1,18 @@
 import Pagination from "components/Pagination";
+import MovieScore from "components/MovieScore";
 
 function Listing(){
 
     return (
+      <>
+        
         <Pagination/>
-
+<MovieScore/>
+   
+     </>
 
     );
 }
 
 export default Listing;
+

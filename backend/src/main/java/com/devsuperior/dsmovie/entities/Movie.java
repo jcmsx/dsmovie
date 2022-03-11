c39f3f6b7ec329fce96c1180ef45583049855561
Juliano Carlos de Oliveira <jcmsx@yahoo.com.br>
2022-03-11 20:59:26 -0300

    Domain model, database seed

--- backend/src/main/java/com/devsuperior/dsmovie/entities/Movie.java
+++ backend/src/main/java/com/devsuperior/dsmovie/entities/Movie.java
@@ -7,7 +7,7 @@ import javax.persistence.Id;
 import javax.persistence.Table;
 
 @Entity
-@Table(name  = "tb-movie")
+@Table(name  = "tb_movie")
 public class Movie {
 	
 	
--- backend/src/main/java/com/devsuperior/dsmovie/entities/ScorePK.java
+++ backend/src/main/java/com/devsuperior/dsmovie/entities/ScorePK.java
@@ -1,14 +1,22 @@
 package com.devsuperior.dsmovie.entities;
 
+import java.io.Serializable;
+
 import javax.persistence.Embeddable;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
 
 @Embeddable
 
-public class ScorePK {
+public class ScorePK implements Serializable {
+	private static final long serialVersionUID = 1L;
 
-	
-	
+	@ManyToOne
+	@JoinColumn(name = "movie_id")
 	private Movie movie;
+	
+	@ManyToOne
+	@JoinColumn(name = "user_id")
 	private User user;
 	
 	public ScorePK() {
